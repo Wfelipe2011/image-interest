0522654ef30344bfca7f8bfdd8c4ddc6f119667a
wilson <wfelipe2011@gmail.com>
2022-06-26 17:11:59 -0300

    init project

---
+++ src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/infra/database/database.module';
+import { UsersModule } from './module/users/users.module';
+@Module({
+  imports: [DatabaseModule, UsersModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
